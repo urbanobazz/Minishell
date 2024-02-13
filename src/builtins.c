ccbf378cf2342787563769f7a8670151142b6259
lodemetz <lodemetz@student.42berlin.de>
2024-02-13 15:53:40 +0100

    unfinished business with cd

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: lodemetz <lodemetz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 14:14:38 by lodemetz          #+#    #+#             */
-/*   Updated: 2024/02/13 15:09:49 by lodemetz         ###   ########.fr       */
+/*   Updated: 2024/02/13 15:52:34 by lodemetz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ void	ft_exit(t_data *data)
 {
 	free_data(data);
 	exit(EXIT_SUCCESS);
+}
+
+void	ft_cd(t_data *data, char **cmds)
+{
+	if (!cmds[1] || chdir(cmds[1]))
+		error_and_quit(data);
 }
 
 int	is_builtin(char *cmd)
