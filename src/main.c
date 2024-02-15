db5242c43388a2997c88adff82d70b2cede896cc
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-02-15 19:06:30 +0100

    minishell function is now a loop

--- src/main.c
+++ src/main.c
@@ -6,27 +6,35 @@
 /*   By: louis.demetz <louis.demetz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2024/02/15 12:54:09 by louis.demet      ###   ########.fr       */
+/*   Updated: 2024/02/15 19:04:10 by louis.demet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "minishell.h"
 
-void	get_user_input(t_data *data)
+int	get_user_input(t_data *data)
 {
+	
 	data->user_input = readline("minishell: ");
+	if (!data->user_input)
+		return (0);
 	add_history(data->user_input);
+	return (1);
 }
 
 void	minishell(t_data *data)
 {
-	reset_data(data);
-	get_user_input(data);
-	lexer(data);
-	parser(data);
-	executor(data);
-	free_data_and_restart(data);
+	while (1)
+	{
+		reset_data(data);
+		if (!get_user_input(data))
+			break ;
+		lexer(data);
+		parser(data);
+		executor(data);
+		free_data(data);
+	}
 }
 
 int	main(int argc, char **argv)
