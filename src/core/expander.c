a71a3e430ebaaa53fb0ff394a9a50cca3acf4060
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-02-16 14:27:39 +0100

    support for $?

--- src/core/expander.c
+++ src/core/expander.c
@@ -6,7 +6,7 @@
 /*   By: louis.demetz <louis.demetz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 18:17:33 by louis.demet       #+#    #+#             */
-/*   Updated: 2024/02/16 12:45:39 by louis.demet      ###   ########.fr       */
+/*   Updated: 2024/02/16 14:24:48 by louis.demet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ char	*find_next_variable(const char *str)
 	p = str;
 	while (*p)
 	{
-		if (*p == '$' && *(p + 1) != '\0' && (ft_isalnum(*(p + 1)) 
-			|| *(p + 1) == '_'))
-			return (char *)p;
+		if (*p == '$' && *(p + 1) != '\0' && (ft_isalnum(*(p + 1))
+				|| *(p + 1) == '_' || *(p + 1) == '?'))
+			return ((char *)p);
 		p++;
 	}
 	return (FAILURE);
@@ -37,14 +37,18 @@ char	*get_variable_value(t_data *data, char *var_start, int var_name_length)
 	char	*value;
 
 	var_name = ft_substr(var_start + 1, 0, var_name_length);
-	value = ft_getenv(data, var_name);
+	if (var_name[0] == '?')
+		value = ft_itoa(data->err_code);
+	else
+		value = ft_getenv(data, var_name);
 	if (!value)
 		value = "";
 	free(var_name);
 	return (value);
 }
 
-char *replace_next_variable(char *result, char *value, char *var_end, char *var_start)
+char	*replace_next_variable(char *result, char *value, char *var_end,
+	char *var_start)
 {
 	char	*new_result;
 
@@ -54,7 +58,7 @@ char *replace_next_variable(char *result, char *value, char *var_end, char *var_
 	return (new_result);
 }
 
-char *replace_variables(t_data *data, const char *str)
+char	*replace_variables(t_data *data, const char *str)
 {
 	char	*result;
 	char	*var_start;
@@ -66,8 +70,13 @@ char *replace_variables(t_data *data, const char *str)
 	{
 		var_start = find_next_variable(result);
 		var_end = var_start + 1;
-		while (isalnum(*var_end) || *var_end == '_')
+		if (*var_end == '?')
 			var_end++;
+		else
+		{
+			while (isalnum(*var_end) || *var_end == '_')
+				var_end++;
+		}
 		value = get_variable_value(data, var_start, var_end - var_start - 1);
 		result = replace_next_variable(result, value, var_end, var_start);
 	}
@@ -75,10 +84,10 @@ char *replace_variables(t_data *data, const char *str)
 	return (result);
 }
 
-void expand_variables_and_remove_quotes(t_data *data)
+void	expand_variables_and_remove_quotes(t_data *data)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (i < data->command_count)
@@ -88,10 +97,11 @@ void expand_variables_and_remove_quotes(t_data *data)
 		{
 			if (data->cmds[i][j][0] != SGL_QUOTE)
 				data->cmds[i][j] = replace_variables(data, data->cmds[i][j]);
-			if (data->cmds[i][j][0] == DBL_QUOTE || data->cmds[i][j][0] == SGL_QUOTE)
+			if (data->cmds[i][j][0] == DBL_QUOTE
+				|| data->cmds[i][j][0] == SGL_QUOTE)
 			{
-				data->cmds[i][j] = ft_substr(data->cmds[i][j],
-											1, 
+				data->cmds[i][j] = ft_substr(data->cmds[i][j], \
+											1, \
 											ft_strlen(data->cmds[i][j]) - 2);
 				if (!data->cmds[i][j])
 					error_and_quit(data, 2);
