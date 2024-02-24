eff9e9b94e8190ca5fec3e4c84cb28ad121f5737
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-02-24 12:40:19 +0100

    cleaned up executor

--- src/core/executor.c
+++ src/core/executor.c
@@ -6,7 +6,7 @@
 /*   By: louis.demetz <louis.demetz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 13:43:43 by louis.demet       #+#    #+#             */
-/*   Updated: 2024/02/24 12:31:05 by louis.demet      ###   ########.fr       */
+/*   Updated: 2024/02/24 12:38:51 by louis.demet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,7 @@ void init_pipes(t_data *data)
 		if (!data->pipes[i])
 			error_and_quit(data, 2);
 		if (pipe(data->pipes[i]) == -1)
-		{
 			error_and_quit(data, 11);
-		}
-		printf("1: Created pipe %d: read end = %d, write end = %d\n", i, data->pipes[i][0], data->pipes[i][1]);
 		i++;
 	}
 }
@@ -70,46 +67,25 @@ void close_all_pipes(t_data *data)
 
 void execute_cmd(t_data *data, int i, int input_fd, int output_fd)
 {
-	printf("4: in subprocess pid %i, preparing for execution, STDIN: %i, STDOUT: %i\n", getpid(), input_fd, output_fd);
-	if (input_fd != STDIN_FILENO)
-	{
-		if (dup2(input_fd, STDIN_FILENO) == -1)
-		{
-			perror("[execute_cmd] dup2 input_fd failed");
-			exit(EXIT_FAILURE);
-		}
-		printf("5: in subprocess pid %i, duplicated fd %i\n", getpid(), input_fd);
-		// printf("6: in subprocess pid %i, closed fd %i\n", getpid(), input_fd);
-		// close(input_fd);
-	}
-	if (output_fd != STDOUT_FILENO)
-	{
-		if (dup2(output_fd, STDOUT_FILENO) == -1)
-		{
-			perror("[execute_cmd] dup2 output_fd failed");
-			exit(EXIT_FAILURE);
-		}
-		printf("5: in subprocess pid %i, duplicated fd %i\n", getpid(), output_fd);
-		// printf("6: in subprocess pid %i, closed fd %i\n", getpid(), output_fd);
-		// close(output_fd);
-	}
-	printf("7: in subprocess pid %i executing command with execve: %s, STDIN: %d, STDOUT: %d\n", getpid(), data->cmds[i][0], STDIN_FILENO, STDOUT_FILENO);
+	if (input_fd != STDIN_FILENO && dup2(input_fd, STDIN_FILENO) == -1)
+			error_and_quit(data, 11);
+	if (output_fd != STDOUT_FILENO && dup2(output_fd, STDOUT_FILENO) == -1)
+			error_and_quit(data, 11);
 	close_all_pipes(data);
 	execve(data->cmd_paths[i], data->cmds[i], data->env);
-	// Note: If execve returns, it has failed
 	error_and_quit(data, 12);
 }
 
 void execute_shell_command_with_redirection(t_data *data, int i)
 {
-	int infile_fd, outfile_fd;
+	int	infile_fd;
+	int	outfile_fd;
 
 	if (i == 0)
 		infile_fd = data->infile_fd;
 	else
 	{
 		infile_fd = data->pipes[i - 1][0];
-		printf("3s: In subprocess pid %i, closed fd %i\n", getpid(), data->pipes[i - 1][1]);
 		close(data->pipes[i - 1][1]);
 	}
 	if (i == data->command_count - 1)
@@ -117,7 +93,6 @@ void execute_shell_command_with_redirection(t_data *data, int i)
 	else
 	{
 		outfile_fd = data->pipes[i][1];
-		printf("3s: In subprocess pid %i, closed fd %i\n", getpid(), data->pipes[i][0]);
 		close(data->pipes[i][0]);
 	}
 	execute_cmd(data, i, infile_fd, outfile_fd);
@@ -126,34 +101,23 @@ void execute_shell_command_with_redirection(t_data *data, int i)
 void fork_subprocess(t_data *data, int i)
 {
 	data->processes[i] = fork();
-	if (data->processes[i] == 0) // Child process
-	{
-		printf("2s: Starting subprocess pid %d for command %d\n", getpid(), i);
+	if (data->processes[i] == 0)
 		execute_shell_command_with_redirection(data, i);
-	}
 	else if (data->processes[i] < 0)
 		error_and_quit(data, 11);
-	else // Parent process
+	else
 	{
-		printf("2m: In main process pid %i, forked subprocess pid %d for command %d\n", getpid(), data->processes[i], i);
-		// Close pipe ends in the parent as soon as they are no longer needed
 		if (i > 0)
-		{
-			printf("3m: In main process pid %i, closed fd %i\n", getpid(), data->pipes[i - 1][0]);
 			close(data->pipes[i - 1][0]);
-		}
 		if (i != data->command_count - 1)
-		{
-			printf("3m: In main process pid %i, closed fd %i\n", getpid(), data->pipes[i][1]);
 			close(data->pipes[i][1]);
-		}
 	}
 }
 
 int run_subprocesses(t_data *data)
 {
 	int i;
-	int ret;
+	int res;
 
 	i = 0;
 	data->processes = ft_calloc(sizeof(pid_t), data->command_count);
@@ -161,10 +125,10 @@ int run_subprocesses(t_data *data)
 		error_and_quit(data, 2);
 	while (i < data->command_count)
 	{
-		ret = find_and_trigger_builtin(data, data->cmds[i]);
-		if (!ret)
+		res = find_and_trigger_builtin(data, data->cmds[i]);
+		if (!res)
 			return (FAILURE);
-		if (ret == NOT_BUILTIN)
+		if (res == NOT_BUILTIN)
 			fork_subprocess(data, i);
 		i++;
 	}
@@ -177,7 +141,6 @@ void wait_for_subprocesses(t_data *data)
 	while (i < data->command_count)
 	{
 		waitpid(data->processes[i++], &status, 0);
-		printf("8: Process %d (PID: %d) exited with status %d\n", i, data->processes[i], WEXITSTATUS(status));
 		if (WIFEXITED(status))
 			data->exit_status = WEXITSTATUS(status);
 	}
