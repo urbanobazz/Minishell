c78488951a622fc370714b1e0e131e0957e6a494
lodemetz <lodemetz@student.42berlin.de>
2024-02-26 15:40:10 +0100

    file descriptor closed

--- src/core/builtin_redirection.c
+++ src/core/builtin_redirection.c
@@ -6,7 +6,7 @@
 /*   By: lodemetz <lodemetz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 15:14:29 by lodemetz          #+#    #+#             */
-/*   Updated: 2024/02/26 15:14:45 by lodemetz         ###   ########.fr       */
+/*   Updated: 2024/02/26 15:39:03 by lodemetz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,10 @@ void	restore_redirection(t_data *data, int i, int io[2], int tmp_fd[2])
 		error_and_quit(data, 11);
 	if (io[1] != STDOUT_FILENO && dup2(tmp_fd[1], STDOUT_FILENO) == -1)
 		error_and_quit(data, 11);
+	if (io[0] > 2)
+		close(io[0]);
+	if (io[1] > 2)
+		close(io[1]);
 	close(tmp_fd[0]);
 	close(tmp_fd[1]);
 }
